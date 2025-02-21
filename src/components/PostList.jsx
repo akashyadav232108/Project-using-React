49404bf2d85ef88e67e57c19ed27e64b5c7b9903
Akash Yadav <akashyadav2872002@gmail.com>
2025-02-21 13:10:30 +0530

    Used useEffect hook

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,21 +1,21 @@
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import Post from "./Post";
 import { PostList as PostListData } from "../store/post-list-store";
 import WelcomeMsg from "./WelcomeMsg";
 
 const PostList = () => {
   const { postList, addInitialPosts } = useContext(PostListData);
-  
-  const handleGetPostsClick = () => {
+  useEffect(() => {
     fetch("https://dummyjson.com/posts")
     .then((res) => res.json())
     .then(data => {addInitialPosts(data.posts);
   });
-  };
-
+  }, []);
+  
+  
   return (
     <>
-      { postList.length === 0 && <WelcomeMsg onGetPostsClick={handleGetPostsClick}/>}
+      { postList.length === 0 && <WelcomeMsg />}
       {postList.map((post) => (
         <Post key={post.id} post={post} />
       ))}
--- src/components/WelcomeMsg.jsx
+++ src/components/WelcomeMsg.jsx
@@ -2,10 +2,7 @@ const WelcomeMsg = ({onGetPostsClick}) => {
   return (
   <center className="welcome-msg">
            <h1 >There are no posts.</h1>
-           <button type="button" 
-           onClick = {onGetPostsClick}
-           className="btn btn-primary">Get Posts From Server</button>
-        </center>
+   </center>
   );
 }
 
